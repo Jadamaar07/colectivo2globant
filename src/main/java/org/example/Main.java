66b29bf5218980d47a6db51fee0562a8b1466cee
Jadamaar07 <javierdavid742000@gmail.com>
2025-07-01 09:04:27 -0500

    Se agrego el Scanner

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
 
-        System.out.println("RESIDEN EVIL");
+        System.out.println("RESIDENT EVIL");
 
-        System.out.println("DEVELOPER JJG");
+        System.out.println("DEVELOPER JDM");
+
 
     }
 }
